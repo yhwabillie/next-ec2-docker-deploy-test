c3594678a5ebe6675bde8b17256c438869de295b
yhwa <yhwabillie93@gmail.com>
2024-07-14 21:08:21 +0900

    배포 캐싱 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,20 @@ export default function Home() {
     <main>
       <h1>도커 캐싱 EC2 깃 액션 테스트</h1>
       <h2>2차 테스트</h2>
+      <h2>3차 테스트 진짜 끝</h2>
+      <ul>
+        <li>Node v20, Yarn Berry v4.x, nodeLinker: node-modules</li>
+        <li>NextJS v14, standalone 빌드 사용</li>
+        <li>Github Actions 워크플로우 작성 ✅</li>
+        <li>
+          Github Actions - 종속성 캐싱, next 빌드 cache 캐싱, 도커 레이어 캐싱,
+          빌드 속도 개선 ✅
+        </li>
+        <li>Docker Dockerfile 작성, 도커 이미지 경량화, 빌드 최적화 ✅</li>
+        <li>Docker Multi-Stage Build ✅</li>
+        <li>Github Container Registry = 도커허브, ECR ✅</li>
+        <li>EC2 배포 ✅</li>
+      </ul>
     </main>
   );
 }
