e6a5a56aafdae59183926d9dff979c9f9eef13b0
yhwa <yhwabillie93@gmail.com>
2024-07-14 20:04:32 +0900

    캐싱 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -9,23 +9,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <h1>캐싱 테스트</h1>
       </div>
 
       <div className={styles.center}>
