26cc1462786189825c11471320e1ed42851e7c0a
yhwa <yhwabillie93@gmail.com>
2024-07-14 20:33:32 +0900

    캐싱 테스트 2차

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,10 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main>도커 캐싱 EC2 깃 액션 테스트</main>;
+  return (
+    <main>
+      <h1>도커 캐싱 EC2 깃 액션 테스트</h1>
+      <h2>2차 테스트</h2>
+    </main>
+  );
 }
